96d10f33143ff5225d64cb930a03d7949878dff8
Tarun Sahni <81422161+kalilinux0179@users.noreply.github.com>
2024-07-17 12:15:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,56 @@ from termcolor import colored
 from time import sleep
 import subprocess
 
-
 hostList = []
+
+
+def createDirectory(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            print(colored("[-] {} Directory Created".format(directory), "cyan"))
+            os.chdir(directory)
+            return True
+        else:
+            os.chdir(directory)
+            return True
+    except OSError:
+        sys.stderr.write(colored("[-] Unable to create {}".format(directory), "red"))
+        sys.stderr.write(colored("Exiting...", "red"))
+
+
+def clearScreen():
+    if sys.platform.startswith("win"):
+        os.system("cls")
+    elif sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
+        os.system("clear")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Subdomain Enumeration Tool",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "-d", "-domain", dest="domainName", metavar="", help="Domain Name"
+    )
+    parser.add_argument(
+        "-dL",
+        "-domains",
+        dest="domainFile",
+        metavar="",
+        help="File Containing multiple Domain Names",
+    )
+    parser.add_argument(
+        "-m",
+        "-modes",
+        dest="modes",
+        metavar="",
+        choices=["subfinder", "assetfinder", "amass", "all"],
+        nargs="+",
+        help="Specify Modes",
+    )
+    return parser.parse_args()
 
 
 class Mode:
@@ -51,6 +99,33 @@ class Mode:
             sys.stderr.write("[!] Exiting...")
             sys.exit()
 
+    def asnmap(self):
+        sleep(1)
+        clearScreen()
+        tool_name = "asnmap"
+        command = "{0} -d {1} -silent | tlsx -san -silent".format(
+            tool_name, self.target
+        )
+        print(colored("[+] Running {0} on {1}".format(tool_name, self.target), "green"))
+        try:
+            process = subprocess.Popen(
+                command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            sys.stdout.write(process.stdout)
+            with open(f"{tool_name}.txt", "a") as file:
+                file.write(process.stdout)
+            process.communicate()
+        except subprocess.CalledProcessError as e:
+            print(f"Command failed with return code {e.returncode}")
+        except KeyboardInterrupt:
+            sys.stderr.write("[!] Pressed CTRL+C")
+            sleep(0.5)
+            sys.stderr.write("[!] Exiting...")
+            sys.exit()
+
+    def amass(self):
+        pass
+
     def allModules(self):
         sleep(1)
         self.subfinder()
@@ -73,56 +148,12 @@ def findSubDomains(target, modes):
             p1.subfinder()
         elif mode == "assetfinder":
             p1.assetfinder()
+        elif mode == "ansmap":
+            p1.asnmap()
+        elif mode == "amass":
+            p1.amass()
         elif mode == "all":
             p1.allModules()
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="Subdomain Enumeration Tool",
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-    parser.add_argument(
-        "-d", "-domain", dest="domainName", metavar="", help="Domain Name"
-    )
-    parser.add_argument(
-        "-dL",
-        "-domains",
-        dest="domainFile",
-        metavar="",
-        help="File Containing multiple Domain Names",
-    )
-    parser.add_argument(
-        "-m",
-        "-modes",
-        dest="modes",
-        metavar="",
-        choices=["subfinder", "assetfinder", "all"],
-        nargs="+",
-        help="Specify Modes",
-    )
-    return parser.parse_args()
-
-
-def createDirectory(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-            print(colored("[-] {} Directory Created".format(directory), "cyan"))
-            os.chdir(directory)
-            return True
-        else:
-            os.chdir(directory)
-            return True
-    except OSError:
-        sys.stderr.write(colored("[-] Unable to create {}".format(directory), "red"))
-        sys.stderr.write(colored("Exiting...", "red"))
-
-def clearScreen():
-    if sys.platform.startswith("win"):
-        os.system("cls")
-    elif sys.platform.startswith("linux") or sys.platform.startswith("darwin"):
-        os.system("clear")
 
 
 def main():
